548ba052162043cf586c4a093ea4c74f90c8bdb9
mjanki1054 <80061704+mjanki1054@users.noreply.github.com>
2024-03-12 02:16:20 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,68 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
+import { config } from "./Config";
+import { PublicClientApplication } from "@azure/msal-browser";
+import {Component} from 'react';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+
+  constructor(props) {
+    super(props);
+    this.state = {
+      error: null,
+      isAuthenticated: false,
+      user: {},
+    };
+    this.login = this.login.bind(this);
+
+    this.publicClientApplication = new PublicClientApplication({
+      auth: {
+        clientId: config.appId,
+        redirectUri: config.redirectUri,
+        authority: config.authority,
+      },
+
+      cache: {
+        cacheLocation: "sessionStorage",
+        storeAuthStateInCookie: true,
+      },
+    });
+  }
+
+  async login() {
+    try {
+      await this.publicClientApplication.loginPopup({
+        scopes: config.scopes,
+        prompt: "select_account",
+      });
+      this.setState({isAuthenticated:true});
+    } catch (err) {
+      this.setState({
+        isAuthenticated: false,
+        user: {},
+        error: err,
+      });
+    }
+  }
+
+  logout() {
+    this.publicClientApplication.logout();
+  }
+  render() {
+    return (
+      <div className="App">
+        <header className="App-header">
+          <img src={logo} alt="react-icon" className="App-logo" />
+          {this.state.isAuthenticated ? (
+            <p>successful logged in</p>
+          ) : (
+            <p>
+              <button onClick={() => this.login()}>Login in</button>
+            </p>
+          )}
+        </header>
+      </div>
+    );
+  }
 }
-
 export default App;
---
+++ src/Config.js
@@ -0,0 +1,8 @@
+export const config = {
+    appId: 'e0713049-1a6e-4365-b768-526da5003cf3',
+    redirectUri : 'https://ad-auth-942b5.firebaseapp.com/__/auth/handler',
+    scopes : [
+        'user.read'
+    ],
+    authority:'https://login.microsoftonline.com/TalnetForge.onmicrosoft.com'
+}
